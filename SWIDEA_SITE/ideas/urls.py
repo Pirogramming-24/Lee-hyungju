9c0d0ecbdad843c31ddd71f0ec99e213360a56a5
Tonyjoo11 <hyungjoo.ree@gmail.com>
2026-01-02 13:36:41 +0900

    [assignment/feat] 2주차 아이디어 사이트 기본 설정

---
+++ SWIDEA_SITE/ideas/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path("", views.idea_list, name="home"),
+    # path("idea/", views.idea_list, name="idea_list"),
+
+    # path("idea/create/", views.idea_create, name="idea_create"),
+    # path("idea/<int:pk>/", views.idea_detail, name="idea_detail"),
+    # path("idea/<int:pk>/edit/", views.idea_update, name="idea_update"),
+    # path("idea/<int:pk>/delete/", views.idea_delete, name="idea_delete"),
+
+    # path("devtool/", views.devtool_list, name="devtool_list"),
+    # path("devtool/create/", views.devtool_create, name="devtool_create"),
+    # path("devtool/<int:pk>/", views.devtool_detail, name="devtool_detail"),
+    # path("devtool/<int:pk>/edit/", views.devtool_update, name="devtool_update"),
+    # path("devtool/<int:pk>/delete/", views.devtool_delete, name="devtool_delete"),
+]
